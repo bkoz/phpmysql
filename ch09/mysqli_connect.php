c8f5702075ebc917e379078bb5053ccfba01602d
Bob Kozdemba <bkozdemba@gmail.com>
2017-03-03 20:20:09 -0600

    added env vars

--- ch09/mysqli_connect.php
+++ ch09/mysqli_connect.php
@@ -5,10 +5,16 @@
 // selects the database, and sets the encoding.
 
 // Set the database access information as constants:
+username=getenv('MYSQL_USER');
+password=getenv('MYSQL_USER');
+localhost=getenv('MYSQL_HOST');
+sitename=getenv('MYSQL_DATABASE);
 DEFINE ('DB_USER', 'username');
 DEFINE ('DB_PASSWORD', 'password');
 DEFINE ('DB_HOST', 'localhost');
 DEFINE ('DB_NAME', 'sitename');
+
+echo "DB_USER = $DB_USER";
 
 // Make the connection:
 $dbc = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ('Could not connect to MySQL: ' . mysqli_connect_error() );
